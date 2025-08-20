b0ad901777c97a2e60dd6888cf5116489b538038
v1xingyue <qixingyue@gmail.com>
2025-08-20 15:42:03 +0800

    custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/md2walrus/",
+  // base: "/md2walrus/",
   plugins: [react()],
   optimizeDeps: {
     include: ["@mysten/walrus", "@mysten/walrus-wasm"],
